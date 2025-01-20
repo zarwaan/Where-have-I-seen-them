f15908e48f1fa03c1dd12db67655bdd65ac3434a
zarwaan <zarwaanshroff@gmail.com>
2025-01-20 21:22:12 +0530

    Added Connection Pool

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -2,16 +2,30 @@ import * as mysql from 'mysql2';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-var conn = mysql.createConnection({
+// var conn = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+const pool = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-conn.connect((err) => {
-    if(err) throw err;
-    console.log("Connected!");
-});
+const conn = {
+    query: (sql,params,callback) => pool.query(sql,params,callback),
+};
+
+// conn.connect((err) => {
+//     if(err) throw err;
+//     console.log("Connected!");
+// });
 
 export {conn};
